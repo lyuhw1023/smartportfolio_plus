3d80457a2ef7c2baba08c51a742072a17c3300f2
lyuhyewon <lyuhw11023@gmail.com>
2025-04-01 23:45:43 +0900

    Feat: 공통 앱바 구현

---
+++ app/src/main/java/com/hyen/smartportfolio_plus/components/CommonAppBar.kt
@@ -0,0 +1,83 @@
+package com.hyen.smartportfolio_plus.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hyen.smartportfolio_plus.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun CommonAppBar(
+    title: String,
+    scaffoldState: ScaffoldState,
+    scope: CoroutineScope
+) {
+    TopAppBar(
+        title = {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+        },
+        // 메뉴 아이콘
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    scope.launch { scaffoldState.drawerState.open() }
+                }
+            ) {
+                Icon(
+                    Icons.Filled.Menu,
+                    contentDescription = "Menu",
+                    tint = Color.White
+                )
+            }
+        },
+        // 로그아웃 아이콘
+        actions = {
+            IconButton(
+                onClick = { /*firebase 로그아웃 기능*/ }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.logout),
+                    contentDescription = "Logout",
+                    modifier = Modifier.size(25.dp),
+                    tint = Color.White
+                )
+            }
+        },
+        backgroundColor = Color(0xFF6495ED),
+        elevation = 8.dp
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CommonAppBarPreview() {
+    CommonAppBar(
+        title = "SmartPortfolio",
+        scaffoldState = rememberScaffoldState(),
+        scope = kotlinx.coroutines.MainScope()
+    )
+}

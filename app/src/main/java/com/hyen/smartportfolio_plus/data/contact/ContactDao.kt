f00bef5398b4c938bf1d15fdb0661dde18acb070
lyuhyewon <lyuhw11023@gmail.com>
2025-04-19 00:02:39 +0900

    Feat: 주석 추가

--- app/src/main/java/com/hyen/smartportfolio_plus/data/contact/ContactDao.kt
+++ app/src/main/java/com/hyen/smartportfolio_plus/data/contact/ContactDao.kt
@@ -8,17 +8,22 @@ import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
+// DB 작업
 @Dao
 interface ContactDao {
+    // 프로젝트 추가
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(contact: Contact)
 
+    // 프로젝트 수정
     @Update
     suspend fun update(contact: Contact)
 
+    // 프로젝트 삭제
     @Delete
     suspend fun delete(contact: Contact)
 
+    // 전체 프로젝트 조회
     @Query("SELECT * FROM contacts ORDER BY timestamp DESC")
     fun getAllContacts(): Flow<List<Contact>>
 }

8cbd5108b84b4ed6b9f0ee2b6e31693fa0c38a38
lyuhyewon <lyuhw11023@gmail.com>
2025-04-29 19:13:14 +0900

    Feat: 화면 이동 설정

--- app/src/main/java/com/hyen/smartportfolio_plus/navigation/NavGraph.kt
+++ app/src/main/java/com/hyen/smartportfolio_plus/navigation/NavGraph.kt
@@ -8,6 +8,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -15,12 +18,14 @@ import com.hyen.smartportfolio_plus.screens.AboutScreen
 import com.hyen.smartportfolio_plus.screens.ContactFormScreen
 import com.hyen.smartportfolio_plus.screens.ContactScreen
 import com.hyen.smartportfolio_plus.screens.HomeScreen
+import com.hyen.smartportfolio_plus.screens.LoadingScreen
 import com.hyen.smartportfolio_plus.screens.LoginScreen
 import com.hyen.smartportfolio_plus.screens.MoreScreen
 import com.hyen.smartportfolio_plus.screens.ProjectFormScreen
 import com.hyen.smartportfolio_plus.screens.ProjectScreen
 import com.hyen.smartportfolio_plus.viewmodel.AuthViewModel
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 
 @Composable
 fun NavGraph(
@@ -31,25 +36,40 @@ fun NavGraph(
     authViewModel: AuthViewModel,
     googleSignInLauncher: ActivityResultLauncher<Intent>
 ) {
-    val startRoute = if (authViewModel.isLoggedIn()) "home" else "login"
     NavHost(
         navController = navController,
-        startDestination = startRoute
+        startDestination = "login"
     ) {
         composable("login") {
             val userId by authViewModel.userIdLiveData.observeAsState()
+            var loading by remember { mutableStateOf(false) }
+
+            // userId 있으면 home으로 (로그인 성공)
             LaunchedEffect(userId) {
-                if (userId != null) {
+                if (userId != null && !loading) {
+                    loading = true
+                }
+            }
+            if (loading) {
+                LoadingScreen()
+                LaunchedEffect(Unit) {
+                    delay(1000L)
                     navController.navigate("home") {
                         popUpTo("login") { inclusive = true }
                     }
                 }
+            } else {
+                LoginScreen(
+                    // 비회원, 홈으로 이동
+                    onGuest = {
+                        loading = true
+                    },
+                    // 로그인 버튼 > Google 로그인
+                    onLogin = {
+                        googleSignInLauncher.launch(authViewModel.getSignInIntent())
+                    }
+                )
             }
-            LoginScreen(
-                onClickSignIn = {
-                    googleSignInLauncher.launch(authViewModel.getSignInIntent())
-                }
-            )
         }
         composable("home") {
             HomeScreen(

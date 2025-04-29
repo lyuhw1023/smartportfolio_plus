08890fbe162e517c5908b5b0df5b6c870e0b8e32
lyuhyewon <lyuhw11023@gmail.com>
2025-04-29 16:35:05 +0900

    Feat: 로그인 화면 UI 수정 및 비회원, 회원 로그인 구분

--- app/src/main/java/com/hyen/smartportfolio_plus/screens/LoginScreen.kt
+++ app/src/main/java/com/hyen/smartportfolio_plus/screens/LoginScreen.kt
@@ -1,27 +1,64 @@
 
 package com.hyen.smartportfolio_plus.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.hyen.smartportfolio_plus.R
+import com.hyen.smartportfolio_plus.ui.theme.primary
+import com.hyen.smartportfolio_plus.ui.theme.primaryClick
 
 @Composable
 fun LoginScreen (
-    onClickSignIn: () -> Unit
+    onGuest: () -> Unit,
+    onLogin: () -> Unit
     ) {
     Column (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "App Logo",
+            modifier = Modifier.size(120.dp)
+        )
+        Text(
+            "Smart Portfolio",
+            style = MaterialTheme.typography.headlineLarge
+            )
+        Spacer(Modifier.height(40.dp))
         Button(
-            onClick = onClickSignIn,
-            modifier = Modifier.padding(16.dp)
+            onClick = onGuest,
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = primary,
+                contentColor = Color.White,
+                disabledContainerColor = primaryClick,
+                disabledContentColor = Color.White
+            )
         ) {
-            Text(text = "Goggle 로그인")
+            Text(text = "둘러보기")
+        }
+        Spacer(Modifier.height(10.dp))
+        Button(
+            onClick = onLogin,
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = primary,
+                contentColor = Color.White,
+                disabledContainerColor = primaryClick,
+                disabledContentColor = Color.White
+            )
+        ) {
+            Text(text = "Google 로그인")
         }
     }
 }

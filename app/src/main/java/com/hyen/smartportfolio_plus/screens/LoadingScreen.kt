215265d025ec69cf235543e35a39f0c3af9be8d9
lyuhyewon <lyuhw11023@gmail.com>
2025-05-06 14:21:03 +0900

    Feat: laodingscreen 구현

---
+++ app/src/main/java/com/hyen/smartportfolio_plus/screens/LoadingScreen.kt
@@ -0,0 +1,29 @@
+package com.hyen.smartportfolio_plus.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.hyen.smartportfolio_plus.ui.theme.primary
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.size(70.dp),
+            color = primary,
+            strokeWidth = 7.dp
+        )
+    }
+}
